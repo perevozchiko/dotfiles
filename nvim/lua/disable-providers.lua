42aed7bf60d85f36763a740e76279698bc7e8bcf
Perevozchikov Andrei <perevozchikov.andrey@gmail.com>
2025-12-13 19:33:04 +0100

    Delete old configs

--- nvim/lua/disable-providers.lua
+++
@@ -1,7 +0,0 @@
--- Disable unused providers to reduce startup warnings
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0  
-vim.g.loaded_ruby_provider = 0
-
-
-
--- nvim/lua/tmux-fix.lua
+++
@@ -1,6 +0,0 @@
--- Fix for tmux true color support
-if vim.env.TMUX then
-  vim.opt.termguicolors = true
-  -- Modern Neovim handles tmux colors automatically
-  -- No need for manual terminal codes in recent versions
-end
