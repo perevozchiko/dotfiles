b7d97628db50f4ea5fb1b005dd892d327a3ad7af
Perevozchikov Andrei <perevozchikov.andrey@gmail.com>
2025-12-13 22:21:56 +0100

    Add git plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -31,4 +31,34 @@ return {
       },
     },
   },
+
+  -- Git integration
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Git Diff" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Git File History" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Git Project History" },
+      { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    },
+  },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+    opts = {
+      integrations = {
+        diffview = true,
+      },
+    },
+  },
 }
